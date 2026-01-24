0cff0687d05079509791be74c930e9c410a8216a
Sujit Yadav <yadavsujit3366@gmail.com>
2026-01-25 00:03:07 +0530

    Create Minimize Maximum Pair Sum in Array.java

---
+++ questions/Arrays/Minimize Maximum Pair Sum in Array.java
@@ -0,0 +1,17 @@
+//Approach: Sorting
+//Time complexity O(NlogN)
+//Space complexity O(logN)
+
+class Solution {
+  public int minPairSum(int[] nums) {
+    Arrays.sort(nums);
+
+    int maxSum = 0;
+    for (int i = 0; i < nums.length / 2; i++) {
+      maxSum = Math.max(maxSum, nums[i] + nums[nums.length - 1 - i]);
+    }
+
+    return maxSum;
+
+  }
+}
