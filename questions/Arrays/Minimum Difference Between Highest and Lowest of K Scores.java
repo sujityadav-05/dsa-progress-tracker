0941a86bcbd2a77d2c6f9a0aec340b09b6bebdeb
Sujit Yadav <yadavsujit3366@gmail.com>
2026-01-26 00:25:28 +0530

    Create Minimum Difference Between Highest and Lowest of K Scores.java

---
+++ questions/Arrays/Minimum Difference Between Highest and Lowest of K Scores.java
@@ -0,0 +1,16 @@
+//Approach: Sorting
+//Time complexity: O(nlogn)
+//Space complexity: O(logn)
+
+class Solution {
+
+    public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i + k - 1 < n; ++i) {
+            ans = Math.min(ans, nums[i + k - 1] - nums[i]);
+        }
+        return ans;
+    }
+}
